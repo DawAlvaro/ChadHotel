756933412043e2ecc4f446e87d19f6c007ea023d
Alvaro Ramirez <91119635+DawAlvaro@users.noreply.github.com>
2022-06-20 14:34:26 +0200

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,23 @@
 <?php
 
+use App\Models\User;
+use App\Events\NewReservationEvent;
+use App\Events\RefreshDashboardEvent;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ChartController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\FacilityController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\NotificationsController;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\RoomController;
+use App\Http\Controllers\TransactionRoomReservationController;
+use App\Http\Controllers\RoomStatusController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TypeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +31,73 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+Route::group(['middleware' => ['auth', 'checkRole:Super']], function () {
+    Route::resource('user', UserController::class);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::group(['middleware' => []], function () {
+    Route::post('/room/{room}/image/upload', [ImageController::class, 'store'])->name('image.store');
+    Route::delete('/image/{image}', [ImageController::class, 'destroy'])->name('image.destroy');
 
-require __DIR__.'/auth.php';
+    Route::name('transaction.reservation.')->group(function () {
+        Route::get('/createIdentity', [TransactionRoomReservationController::class, 'createIdentity'])->name('createIdentity');
+        Route::get('/pickFromCustomer', [TransactionRoomReservationController::class, 'pickFromCustomer'])->name('pickFromCustomer');
+        Route::post('/storeCustomer', [TransactionRoomReservationController::class, 'storeCustomer'])->name('storeCustomer');
+        Route::get('/{customer}/viewCountPerson', [TransactionRoomReservationController::class, 'viewCountPerson'])->name('viewCountPerson');
+        Route::get('/{customer}/chooseRoom', [TransactionRoomReservationController::class, 'chooseRoom'])->name('chooseRoom');
+        Route::get('/{customer}/{room}/{from}/{to}/confirmation', [TransactionRoomReservationController::class, 'confirmation'])->name('confirmation');
+        Route::post('/{customer}/{room}/payDownPayment', [TransactionRoomReservationController::class, 'payDownPayment'])->name('payDownPayment');
+    });
+
+    Route::resource('customer', CustomerController::class);
+    Route::resource('type', TypeController::class);
+    Route::resource('room', RoomController::class);
+    Route::resource('roomstatus', RoomStatusController::class);
+    Route::resource('transaction', TransactionController::class);
+    Route::resource('dashboard', TransactionController::class);
+    Route::resource('facility', FacilityController::class);
+
+    Route::get('/payment', [PaymentController::class, 'index'])->name('payment.index');
+    Route::get('/payment/{payment}/invoice', [PaymentController::class, 'invoice'])->name('payment.invoice');
+
+    Route::get('/transaction/{transaction}/payment/create', [PaymentController::class, 'create'])->name('transaction.payment.create');
+    Route::post('/transaction/{transaction}/payment/store', [PaymentController::class, 'store'])->name('transaction.payment.store');
+
+    Route::get('/get-dialy-guest-chart-data', [ChartController::class, 'dialyGuestPerMonth']);
+    Route::get('/get-dialy-guest/{year}/{month}/{day}', [ChartController::class, 'dialyGuest'])->name('chart.dialyGuest');
+});
+
+Route::group(['middleware' => ['auth', 'checkRole:Super,Admin,Customer']], function () {
+    Route::resource('user', UserController::class)->only([
+        'show'
+    ]);
+
+    Route::view('/notification', 'notification.index')->name('notification.index');
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/mark-all-as-read', [NotificationsController::class, 'markAllAsRead'])->name('notification.markAllAsRead');
+
+    Route::get('/notification-to/{id}',[NotificationsController::class, 'routeTo'])->name('notification.routeTo');
+});
+
+Route::view('/login', 'auth.login')->name('login');
+Route::post('/postLogin', [AuthController::class, 'postLogin'])->name('postlogin');
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/sendEvent', function () {
+    $superAdmins = User::where('role', 'Super')->get();
+    event(new RefreshDashboardEvent("Someone reserved a room"));
+
+    foreach ($superAdmins as $superAdmin) {
+        $message = 'Reservation added by';
+        // event(new NewReservationEvent($message, $superAdmin));
+    }
+});
